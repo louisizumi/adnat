2cc9fb8b06ee116bf304a22262f83c9a47897919
Louis Drinkwater <izumilouis95@gmail.com>
2021-12-04 14:02:14 +0000

    Add associations and validations to models

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,2 +1,5 @@
 class Organisation < ApplicationRecord
+  has_many :users
+
+  validates :name, :hourly_rate, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :shifts
+  belongs_to :organisation
+
+  validates :full_name, presence: true, format: { with: /\A([a-z]+[ \-.,']* )+\z/i }
 end
