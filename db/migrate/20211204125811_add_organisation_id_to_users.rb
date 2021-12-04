1d2eb593e681eaec9ce2d238555d8354f5102f1a
Louis Drinkwater <izumilouis95@gmail.com>
2021-12-04 14:25:42 +0000

    Remove extra colon from migration

--- db/migrate/20211204125811_add_organisation_id_to_users.rb
+++ db/migrate/20211204125811_add_organisation_id_to_users.rb
@@ -1,5 +1,5 @@
 class AddOrganisationIdToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_reference :users, :organisation, :foreign_key: true 
+    add_reference :users, :organisation, foreign_key: true 
   end
 end
