7446a8af25f6bc75a1e11d717449e84abf54c767
Louis Drinkwater <izumilouis95@gmail.com>
2021-12-06 03:02:39 +0000

    Account for sunday penalty rates

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -10,4 +10,17 @@ class Shift < ApplicationRecord
   def hours
     (self.finish - self.start - (self.break * 60)) / 3600
   end
+
+  def sunday_hours
+    if self.start.wday == 6
+      @sunday_hours = self.start - self.finish.midnight
+      if @sunday_hours >= 0
+        self.hours
+      else
+        @sunday_hours / 3600
+      end
+    else
+      0
+    end
+  end
 end
