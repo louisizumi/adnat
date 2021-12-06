2267421d285243ee8c0752feacf23ef12016a904
Louis Drinkwater <izumilouis95@gmail.com>
2021-12-06 01:31:14 +0000

    Implement filters for shifts page

--- app/javascript/controllers/shifts_controller.js
+++ app/javascript/controllers/shifts_controller.js
@@ -1,41 +1,106 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "currentShifts", "previousShifts" ]
-
+  static targets = [ "startDate", "finishDate", "searchForm", "input", "table" ]
+  
+  showRows(value) {
+    const currentShifts = document.querySelectorAll("[data-current='false']");
+    const previousShifts = document.querySelectorAll("[data-current='true']");
+    if (value === "previous") {
+      currentShifts.forEach(row => row.style.display = "none");
+      previousShifts.forEach(row => row.style.display = "table-row");
+    } else {
+      currentShifts.forEach(row => row.style.display = "table-row");
+      previousShifts.forEach(row => row.style.display = "none");
+    }
+  }
+  
   showCurrent(e) {
     e.preventDefault();
-    this.currentShiftsTarget.style.display = "block";
-    this.previousShiftsTarget.style.display = "none";
+    this.tableTarget.setAttribute("data-show", "current");
+    this.showRows(this.tableTarget.dataset.show);
   }
-
+  
   showPrevious(e) {
     e.preventDefault();
-    this.currentShiftsTarget.style.display = "none";
-    this.previousShiftsTarget.style.display = "block";
+    this.tableTarget.setAttribute("data-show", "previous");
+    this.showRows(this.tableTarget.dataset.show);
+  }
+
+  filterDate(e) {
+    e.preventDefault();
+    const url = `${this.searchFormTarget.action}?start_date=${this.startDateTarget.value}&finish_date=${this.finishDateTarget.value}`
+    fetch(url, { headers: { 'Accept': 'text/plain' } })
+    .then(response => response.text())
+    .then((data) => {
+      this.tableTarget.innerHTML = data;
+    })
+    .then(() => {
+      this.showRows(this.tableTarget.dataset.show);
+    });
+  }
+
+  clearDate(e) {
+    e.preventDefault();
+    const url = `${this.searchFormTarget.action}`
+    fetch(url, { headers: { 'Accept': 'text/plain' } })
+    .then(response => response.text())
+    .then((data) => {
+      this.tableTarget.innerHTML = data;
+    })
+    .then(() => {
+      this.startDateTarget.value = null;
+      this.finishDateTarget.value = null;
+      this.showRows(this.tableTarget.dataset.show);
+    });
+  }
+  
+  search(e) {
+    const url = `${this.searchFormTarget.action}?query=${this.inputTarget.value}`
+    fetch(url, { headers: { 'Accept': 'text/plain' } })
+    .then(response => response.text())
+    .then((data) => {
+      this.tableTarget.innerHTML = data;
+    })
+    .then(() => {
+      this.showRows(this.tableTarget.dataset.show);
+    });
+  }
+  
+  sortColumn(e) {
+    e.preventDefault();
+    console.log(e.target.dataset);
+    const url = `${this.searchFormTarget.action}?sort=${e.target.dataset.sort}&order=${e.target.dataset.order}`;
+    fetch(url, { headers: { 'Accept': 'text/plain' } })
+    .then(response => response.text())
+    .then((data) => {
+      this.tableTarget.innerHTML = data;
+    })
+    .then(() => {
+      e.target.dataset.order = e.target.dataset.order === "ASC" ? "DESC" : "ASC";
+      this.showRows(this.tableTarget.dataset.show);
+    });
   }
 
   showForm(e) {
     e.preventDefault();
-    const row = this.getRow(e.target.dataset.id);
-    const form = this.getForm(e.target.dataset.id);
+    let row, form;
+    [row, form] = this.getRows(e.target.dataset.id);
     row.style.display = "none";
     form.style.display = "table-row";
   }
 
   hideForm(e) {
     e.preventDefault();
-    const row = this.getRow(e.target.dataset.id);
-    const form = this.getForm(e.target.dataset.id);
+    let row, form;
+    [row, form] = this.getRows(e.target.dataset.id);
     row.style.display = "table-row";
     form.style.display = "none";
   }
 
-  getRow(id) {
-    return document.querySelector(`[data-rowid='${id}']`);
-  }
-
-  getForm(id) {
-    return document.querySelector(`[data-formid='${id}']`);
+  getRows(id) {
+    const row = document.querySelector(`[data-rowid='${id}']`);
+    const form = document.querySelector(`[data-formid='${id}']`);
+    return [row, form];
   }
 }
